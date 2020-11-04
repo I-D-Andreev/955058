c8a896bec86d5b8cac6a48e8f595d297ed4748e0
Ivan <iivvaann.andreev@gmail.com>
2020-11-04 10:42:13 +0000

    user todos

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,10 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            // $table->string('public_name');
+            // posts
+            // comments
         });
     }
 
