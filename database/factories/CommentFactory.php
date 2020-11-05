03d4e3c4002c06961d3be77187be8f99c3932555
Ivan <iivvaann.andreev@gmail.com>
2020-11-05 06:05:45 +0000

    Better seeding

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,12 +2,13 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+
 use App\Comment;
 use Faker\Generator as Faker;
 
 $factory->define(Comment::class, function (Faker $faker) {
     return [
         'text'=> $faker->text(100),
-        'post_id'=>1,
+        'post_id'=>$faker->numberBetween(1, App\Post::count()),
     ];
 });
