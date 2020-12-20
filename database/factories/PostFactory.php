97f462d28185e3aa35bb11d23d3862b255842a9b
Ivan <iivvaann.andreev@gmail.com>
2020-12-20 15:56:19 +0200

    Count posts in their titles

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -6,8 +6,9 @@ use App\Post;
 use Faker\Generator as Faker;
 
 $factory->define(Post::class, function (Faker $faker) {
+    static $count=2;
     return [
-        'title'=> $faker->randomElement(["Empty Title", "No title", "Hello World"]),
+        'title'=> ($faker->randomElement(["Empty Title", "No title", "Hello World"])." ".$count++),
         'text'=> $faker->text(250),
         'user_id'=>$faker->numberBetween(1, App\User::count()),
     ];
