ac6e5391e735733f7ff7fd0caf03fb6f72657baf
Ivan <iivvaann.andreev@gmail.com>
2020-12-23 14:38:57 +0200

    Post preview + Leave a comment card

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,9 +1,41 @@
 @extends("layouts.app")
 
+@push('imports')
+<link href="{{ asset('css/colours.css') }}" rel="stylesheet">
+@endpush
 
 @section("content")
+<div class="container h-100">
+    <div class="row">
+        <div class="w-100">
+            <h1 class="ml-2">{{$post->title}}</h1>
+        </div>
+        <p class="h4 ml-2 w-100 text-secondary"> by {{$post->author->name}}</p>
+        <hr class="w-100 m-0 col-gray">
+        <p class="h6 p-2 w-50 m-0"> Posted on: {{$post->created_at}}</p>
+        <p class="h6 p-2 w-50 m-0 text-right"> Last updated: {{$post->updated_at}}</p>
+        <hr class="w-100 m-0 p-0 col-gray">
     
-    <h1>{{$post->title}}</h1>   
+    </div>
+    <div class="mt-2">
+        {{$post->text}}
+    </div>
+
+    <div class="row">
+        <hr class="w-100 mt-3 p-0 col-gray">
+    </div>
+
+    <div class="card p-1 mt-1">
+        <h5 class="card-header">Leave a Comment:</h5>
+        <div class="card-body">
+          <form>
+              <textarea class="form-control" rows="3" style="resize: none"></textarea>
+              <button type="submit" class="btn btn-primary float-right mt-3">Submit</button>
+          </form>
+        </div>
+    </div>
+</div>
+    {{-- <h1>{{$post->title}}</h1>   
     <p>Author: {{$post->author->name}}</p>
     <br>
     <div>{{$post->text}}</div> 
@@ -29,12 +61,12 @@
                 <button @click="createComment" >Comment</button>
             </div>
         </div>
-        </div>
+        </div> --}}
 
  @endsection
 
 
- @section('code')
+ {{-- @section('code')
     <script>
         var token = "<?php echo (Auth::user())->api_token; ?>";
         
@@ -79,4 +111,4 @@
         });
     </script>
  @endsection
-
+ --}}
