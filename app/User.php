5ffb9ae024d473736446ac4d07440438a13f9e88
Ivan <iivvaann.andreev@gmail.com>
2020-12-27 18:53:17 +0000

    isAdmin method

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\UserTypes;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -47,5 +48,9 @@ class User extends Authenticatable
 
     public function profile(){
         return $this->hasOne('App\Profile');
+    }
+
+    public function isAdmin(){
+        return $this->type === UserTypes::$admin;
     }
 }
