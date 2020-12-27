c50023556b0385f5d82108e6641f49ca3c5569f3
Ivan <iivvaann.andreev@gmail.com>
2020-12-27 17:01:01 +0000

    Fix constraint violation error

--- database/seeds/TagSeeder.php
+++ database/seeds/TagSeeder.php
@@ -17,13 +17,14 @@ class TagSeeder extends Seeder
         $tag->save();
         $tag->posts()->attach(1);
         
-        // Some tags are also created during Post creation
         factory(App\Tag::class, 10)->create();
         
         $posts = App\Post::get();
 
-
-        Tag::get()->each(function($tag) use ($posts){
+        Tag::get()->except(1)->each(function($tag) use ($posts){
+            // Attach a random number of posts (1 - PostCount/3) to each tag except the first.
+            // Do not attach posts to the first tag so as not to violate the unique
+            // constaint (tagId-postId) if we randomly choose to insert 1-1 again (as it is already added manually).
             $tag->posts()->attach(
                 $posts->random(rand(1, intdiv(App\Post::count(),3)))->pluck('id')->toArray()
             );
