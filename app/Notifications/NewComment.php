179a8d17aa986cc56c2948646e456e73eeb078a7
Ivan <iivvaann.andreev@gmail.com>
2020-12-28 19:55:51 +0000

    Queue notifications

--- app/Notifications/NewComment.php
+++ app/Notifications/NewComment.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class NewComment extends Notification
+class NewComment extends Notification implements ShouldQueue
 {
     use Queueable;
 
