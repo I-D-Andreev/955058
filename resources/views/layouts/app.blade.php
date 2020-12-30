a41d1aa38c014fe376f57e6f19e70375f1a8bf6a
Ivan <iivvaann.andreev@gmail.com>
2020-12-30 18:03:28 +0000

    Move twitter to bottom

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,9 +27,6 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <a>
-                    {{-- {{$twitter->tweet()}} --}}
-                </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -116,6 +113,7 @@
         <div class="row w-100">
             <div class="col-md-3">
                 @yield('content-left')
+                {{$twitter->tweet()}}
             </div>
 
             <div class="col-md-6">
