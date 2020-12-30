bb295ebdde6e09f1a19637992a45392f71e1387d
Ivan <iivvaann.andreev@gmail.com>
2020-12-30 19:06:24 +0000

    Make a request and get news

--- app/NewsAPI.php
+++ app/NewsAPI.php
@@ -1,7 +1,10 @@
 <?php
 
-use App\News;
 namespace App;
+
+use App\News;
+use Illuminate\Support\Facades\Http;
+
 
 class NewsAPI {
     private $newsSite;
@@ -13,7 +16,7 @@ class NewsAPI {
     public function __construct(){
         $this->newsSite = 'http://newsapi.org/v2/top-headlines?';
         $this->apiKey = env('NEWS_API_KEY');
-        $this->pageSize = 5;
+        $this->pageSize = 10;
         $this->country = 'gb';
 
         $this->updateIntervalMinutes = 15;
@@ -21,21 +24,17 @@ class NewsAPI {
 
 
     public function getNews(){
-        return $this->buildAPICall();
+        $news = News::findOrFail(1);
 
-        // $news = News::findOrFail(1);
+        $current = new \DateTime();
+        $newsDateTime = new \DateTime($news->updated_at);
 
-        // $current = new \DateTime();
-        // $newsDateTime = new \DateTime($news->updated_at);
-
-        // $diff = $newsDateTime->diff($current);
-        // if(strlen($news->json)==0 || $diff->i >= $updateIntervalMinutes){
-        //     // return $this->updateNews($news)->json;
-        //     return "need update";
-        // } else {
-        //     // return $news->json;
-        //     return "all good";
-        // }
+        $diff = $newsDateTime->diff($current);
+        if(strlen($news->json)==0 || $diff->i >= $this->updateIntervalMinutes){
+            return $this->updateNews($news)->json;
+        } else {
+            return $news->json;
+        }
     }
 
     private function buildAPICall(){
@@ -43,6 +42,10 @@ class NewsAPI {
     }
 
     private function updateNews($news) {
+        $response = Http::get($this->buildAPICall());
+        $news->json = $response->body();
+        $news->save();
+
         return $news;
     }
 }
