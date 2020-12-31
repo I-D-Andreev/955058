6353aa578f5d31627d2c6d24024533f7b57b630e
Ivan <iivvaann.andreev@gmail.com>
2020-12-31 16:55:08 +0000

    Require authentication to acess the profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,11 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function showCurrentUser()
     {
         return view('users.show', ['user' => Auth::user()]);
